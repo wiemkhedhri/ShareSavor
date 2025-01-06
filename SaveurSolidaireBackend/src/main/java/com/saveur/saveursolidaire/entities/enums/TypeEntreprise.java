dd853a374fe2df0c0be511a74ac53eba548fa87c
wiemkhedhri <wiem.khedri50@gmail.com>
2025-01-06 21:48:47 +0100

    Removed SaveurSolidaireBackend folder from frontend branch

--- SaveurSolidaireBackend/src/main/java/com/saveur/saveursolidaire/entities/enums/TypeEntreprise.java
+++
@@ -1,7 +0,0 @@
-package com.saveur.saveursolidaire.entities.enums;
-
-public enum TypeEntreprise {
-    HOTEL,
-    grocerystore,
-    RESTAURANT
-}
--- SaveurSolidaireBackend/src/main/java/com/saveur/saveursolidaire/entities/enums/UserRole.java
+++
@@ -1,8 +0,0 @@
-package com.saveur.saveursolidaire.entities.enums;
-
-public enum UserRole {
-    ADMINISTRATEUR,
-    ORGANISATIONREPRESENTATIVE,
-    BUISINESSREPRENSETIVE
-
-}
